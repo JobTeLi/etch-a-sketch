859016f845e8f4463689e8cd4ff6c29e63de461d
JobTeLi <job.lindert@gmail.com>
2023-10-30 13:20:59 +0100

    logic for making a 16x16 grid

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+function makeGrid(size){
+  for(let i = 0; i < size * size; i++){  
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('square');
+    container.appendChild(newDiv);
+  };   
+};
+makeGrid(16);
